0c38c0159003d793683a7de97f7160fa079b71f1
delta-psi <deltapsi.exe@proton.me>
2026-06-26 19:03:50 -0400

    Modified zen config

--- Modules/Browser/zen-browser.nix
+++ Modules/Browser/zen-browser.nix
@@ -1,41 +1,9 @@
 
-{ inputs, ... }: 
+{ inputs, pkgs, ... }: 
 
 let
   firefox-addons = inputs.firefox-addons.packages.${pkgs.stdenv.hostPlatform.system};
-  pins = {
-    "Email" = {
-      id = "9d8a8f91-7e29-4688-ae2e-da4e49d4a179";
-      url = "https://mail.protonmail.com";
-      position = 101;
-      isEssential = true;
-    };
-    "GitHub" = {
-      id = "48e8a119-5a14-4826-9545-91c8e8dd3bf6";
-      url = "https://github.com";
-      position = 102;
-    };
-    "Dev Tools" = {
-      id = "d85a9026-1458-4db6-b115-346746bcc692";
-      isGroup = true;
-      isFolderCollapsed = false;
-      editedTitle = true;
-      position = 200;
-      folderIcon = "chrome://browser/skin/zen-icons/selectable/eye.svg";
-    };
-    "NixOS Packages" = {
-      id = "f8dd784e-11d7-430a-8f57-7b05ecdb4c77";
-      url = "https://search.nixos.org/packages";
-      folderParentId = pins."Dev Tools".id;
-      position = 201;
-    };
-    "NixOS Options" = {
-      id = "92931d60-fd40-4707-9512-a57b1a6a3919";
-      url = "https://search.nixos.org/options";
-      folderParentId = pins."Dev Tools".id;
-      position = 202;
-    };
-  };
+
 in {
   imports = [
     inputs.zen-browser.homeModules.twilight-official
@@ -58,7 +26,7 @@ in {
       DisablePocket = true;
       DisableTelemetry = true;
       DontCheckDefaultBrowser = true;
-      NoDefaultBookmarks = true;
+      # NoDefaultBookmarks = true;
       OfferToSaveLogins = false;
       EnableTrackingProtection = {
         Value = true;
@@ -73,7 +41,40 @@ in {
 
         pinsForce = true;
         pinsForceAction = "remove"; # omit or "demote" to keep undeclared pins as normal tabs
-        inherit pins;
+        # inherit pins;
+        pins = {
+          "Email" = {
+            id = "9d8a8f91-7e29-4688-ae2e-da4e49d4a179";
+            url = "https://mail.protonmail.com";
+            position = 101;
+            isEssential = true;
+          };
+          "GitHub" = {
+            id = "48e8a119-5a14-4826-9545-91c8e8dd3bf6";
+            url = "https://github.com";
+            position = 102;
+          };
+          "Dev Tools" = {
+            id = "d85a9026-1458-4db6-b115-346746bcc692";
+            isGroup = true;
+            isFolderCollapsed = false;
+            editedTitle = true;
+            position = 200;
+            folderIcon = "chrome://browser/skin/zen-icons/selectable/eye.svg";
+          };
+          "NixOS Packages" = {
+            id = "f8dd784e-11d7-430a-8f57-7b05ecdb4c77";
+            url = "https://search.nixos.org/packages";
+            # folderParentId = pins."Dev Tools".id;
+            position = 201;
+          };
+          "NixOS Options" = {
+            id = "92931d60-fd40-4707-9512-a57b1a6a3919";
+            url = "https://search.nixos.org/options";
+            # folderParentId = pins."Dev Tools".id;
+            position = 202;
+          };
+        };
 
         containersForce = false; # Delete containers not declared here
         containers = {
