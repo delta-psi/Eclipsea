d1a3fd3df65a8b4e0b7b2ff6975874361a15c5be
delta-psi <deltapsi.exe@proton.me>
2026-07-31 14:28:46 -0400

    removed nixvim follows in flake

--- flake.nix
+++ flake.nix
@@ -57,7 +57,7 @@
     # Nixvim
     nixvim = {
       url = "github:nix-community/nixvim";
-      inputs.nixpkgs.follows = "nixpkgs";
+      # inputs.nixpkgs.follows = "nixpkgs";
     };
 
     # Matugen
