f74845508086d9504b379468d874b31ed75d29a4
delta-psi <deltapsi.exe@proton.me>
2026-07-31 10:16:04 -0400

    wl-copy will be the death of this mac mini

--- Modules/Editors/Neovim/clipboard.nix
+++ Modules/Editors/Neovim/clipboard.nix
@@ -5,8 +5,8 @@
   clipboard = {
     register = "unnamedplus";
     providers = {
-      wl-copy.enable = pkgs.stdenv.isLinux;
       pbcopy.enable = pkgs.stdenv.isDarwin;
+      wl-copy.enable = pkgs.stdenv.isLinux;
     };
   }; 
 }
