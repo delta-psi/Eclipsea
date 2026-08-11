e6aed2a4731791797c1869a371acb0f490cf635c
delta-psi <deltapsi.exe@proton.me>
2026-08-11 14:08:46 -0400

    Changed ssh settings on NixOS side - is this blocking password entry to the server?

--- System/NixOS/services.nix
+++ System/NixOS/services.nix
@@ -14,8 +14,8 @@
     openssh = {
       enable = true;
       settings = {
-        PasswordAuthentication = false;
-        KbdInteractiveAuthentication = false;
+        PasswordAuthentication = true;
+        KbdInteractiveAuthentication = true;
       };
     };
 
