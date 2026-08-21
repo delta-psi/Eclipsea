19b07e35b2501c240ad6fafab36ca2de3191c3da
delta-psi <deltapsi.exe@proton.me>
2026-08-21 11:28:40 -0400

    Added cord.nvim for discord presence

---
+++ Modules/Editors/Neovim/Plugins/cord.nix
@@ -0,0 +1,25 @@
+
+{
+  cord = {
+    enable = true;
+    settings = {
+      display = {
+        flavor = "accent";
+        theme = "catppuccin";
+      };
+      editor = {
+        tooltip = "Neovim";
+      };
+      idle = {
+        enabled = true;
+        timeout = 900000;
+      };
+      text = {
+        workspace = "";
+      };
+      timestamp = {
+        reset_on_idle = true;
+      };
+    };
+  };
+}
