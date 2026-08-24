a8d8f9269344e401a2c9c1f2dca920907df174cc
delta-psi <deltapsi.exe@proton.me>
2026-08-24 11:49:45 -0400

    Changed spicetify theming

--- Modules/Music/spicetify.nix
+++ Modules/Music/spicetify.nix
@@ -16,7 +16,26 @@ in {
       hidePodcasts
       shuffle
     ];
+    theme = spicePkgs.themes.comfy;
+    colorScheme = "catppuccin-macchiato";
+    
+    # colorScheme = "rose-pine-moon";
     # theme = spicePkgs.themes.hazy;
+    # theme = {
+      # name = "Comfy";
+      # src = pkgs.fetchFromGitHub {
+      #   owner = "Comfy-Themes";
+      #   repo = "spicetify";
+      #   rev = "32ff101";
+      #   hash = "sha256-sqvmSXJMLE2in/cB8ZIJE/t4J5D0PKRddWECdYJjgX0=";
+      # };
+      # injectCss = true;
+      # injectThemeJs = true;
+      # replaceColors = true;
+      # homeConfig = true;
+      # overwriteAssets = false;
+      # additionalCss = "";
+    # };
   };
   
 }
